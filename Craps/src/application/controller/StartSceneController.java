768037cef6382e11b100d61b7b827d54a2ca047b
Cary Joe Almanza <cdb940@my.utsa.edu>
2021-11-08 20:52:19 -0600

    got the rule view working with links, going to immediately push this to main, as we are probably never going to touch this

--- Craps/src/application/controller/StartSceneController.java
+++ Craps/src/application/controller/StartSceneController.java
@@ -1,10 +1,13 @@
 package application.controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -50,8 +53,16 @@ public class StartSceneController {
     }
 
     @FXML
-    void toRules(ActionEvent event) {
-
+    void toRules(ActionEvent event) throws IOException {
+    	Object source = event.getSource();
+    	if(source == rulesButton) {
+            URL url = new File("src/application/view/Rules.fxml").toURI().toURL();
+	    	startPane = FXMLLoader.load(url);
+            Scene scene = new Scene(startPane);
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+    	}
     }
 
 }
