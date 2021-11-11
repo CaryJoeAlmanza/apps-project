e21289c3c793d7c8675eef66b71f47602e1a961c
Cary Joe Almanza <cdb940@my.utsa.edu>
2021-11-10 20:59:47 -0600

    added Chip.java class to handle chip info, got drag/drop working

--- Craps/src/application/controller/BoardController.java
+++ Craps/src/application/controller/BoardController.java
@@ -1,10 +1,19 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.model.Chip;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
@@ -234,10 +243,83 @@ public class BoardController {
 
     @FXML
     private Label fourComeLabel;
+    
+    @FXML
+    private ImageView chip$;
 
     @FXML
     void rollDice(ActionEvent event) {
 
+    }
+    
+    Chip dollar1 = new Chip("1", 1, chip1);
+    Chip dollar5 = new Chip("5", 5, chip5);
+    Chip dollar10 = new Chip("10", 10, chip10);
+    Chip dollar25 = new Chip("25", 25, chip25);
+    Chip dollar50 = new Chip("50", 50, chip50);
+    Chip dollar100 = new Chip("100", 100, chip100);
+    Chip totalChip = new Chip("$", 0, chip$);
+    Chip sourceChip = new Chip();
+    
+    public void initialize() {
+    	
+    }
+    
+    @FXML
+    void handleDragDetected(MouseEvent event) throws IOException {
+    	Object source = event.getSource();
+    	dollar1.setChipImg(chip1);
+    	dollar5.setChipImg(chip5);
+    	dollar10.setChipImg(chip10);
+    	dollar25.setChipImg(chip25);
+    	dollar50.setChipImg(chip50);
+    	dollar100.setChipImg(chip100);
+    	totalChip.setChipImg(chip$);
+    	if( source == chip1 ) {
+    		//Chip sourceImg = new Chip("1", 1, chip1);
+    		sourceChip.setChipID("1");
+    		totalChip.setChipValue(dollar1.getChipValue() + totalChip.getChipValue());
+//    		System.out.println(dollar1.getChipValue());
+//    		System.out.println(totalChip.getChipValue());
+//    		System.out.println(totalChip.getChipImg());
+    		Dragboard db = chip1.startDragAndDrop(TransferMode.ANY);
+        	
+        	ClipboardContent cb = new ClipboardContent();
+        	
+        	cb.putImage(totalChip.getChipImg().getImage());
+        	
+    	    db.setContent(cb);
+    	    event.consume();
+    	}
+    }
+
+    @FXML
+    void handleDragOver(DragEvent event) {
+    	if(event.getDragboard().hasImage()) {
+    		event.acceptTransferModes(TransferMode.ANY);
+    	}
+    }
+
+    @FXML
+    void handleDrop(DragEvent event) {
+    	dollar1.setChipImg(chip1);
+    	dollar5.setChipImg(chip5);
+    	dollar10.setChipImg(chip10);
+    	dollar25.setChipImg(chip25);
+    	dollar50.setChipImg(chip50);
+    	dollar100.setChipImg(chip100);
+    	totalChip.setChipImg(chip$);
+    	Image img = event.getDragboard().getImage();
+    	//sourceChip.getChipID();
+    	Object source = event.getSource();
+    	if(source == passLinePane1 || source == passLinePane2) {
+    		//passLineImage.setImage(img);
+    		if( sourceChip.getChipID().equals(dollar1.getChipID())) {
+    			passLineImage.setImage(img);
+    			passLineLabel.setText("" + totalChip.getChipValue());
+    		}
+    	}
+    	
     }
 
 }
