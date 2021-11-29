6d84c459c9e328e6c8f76d23377263189dde3468
Cary Joe Almanza <cdb940@my.utsa.edu>
2021-11-29 16:25:27 -0600

    final submission, included presentation, UML, comments

--- Craps/src/application/controller/StartSceneController.java
+++ Craps/src/application/controller/StartSceneController.java
@@ -16,6 +16,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class StartSceneController {
+	//this is the Home Screen for the game
+	//It will ask you how much money you would like to bet during the game and it will return it as getStartingCash()
+	//It also has a button that will take you to a rules screen
+	//Then will let you play the game
 
     @FXML
     private Button rulesButton;
@@ -40,14 +44,14 @@ public class StartSceneController {
     	Object source = event.getSource();
     	if(source == playButton) {
     		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(StartSceneController.class.getResource("../../application/view/Board.fxml"));
+    		loader.setLocation(StartSceneController.class.getResource("../../application/view/Board.fxml")); //take you to the main board screen to play the game
     		AnchorPane startPane = (AnchorPane) loader.load();
     		BoardController controller = loader.getController();
     		try {
-    			Integer.parseInt(balanceField.getText());
+    			Integer.parseInt(balanceField.getText());  
     		}
     		catch (NumberFormatException e){
-    			errorLabel.setText("Error: please enter a whole number greater than 0");
+    			errorLabel.setText("Error: please enter a whole number greater than 0");			//this is where the starting amount is entered and also makes the amount is over 0
     			balanceField.setText("0");
     		}	
     		if( balanceField.getText().isEmpty() || Integer.parseInt(balanceField.getText()) <= 0 ) {
@@ -71,7 +75,7 @@ public class StartSceneController {
     void toRules(ActionEvent event) throws IOException {
     	Object source = event.getSource();
     	if(source == rulesButton) {
-            URL url = new File("src/application/view/Rules.fxml").toURI().toURL();
+            URL url = new File("src/application/view/Rules.fxml").toURI().toURL();  //will take you to the rules screen
 	    	startPane = FXMLLoader.load(url);
             Scene scene = new Scene(startPane);
             Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
--- Craps/src/application/model/Game.java
+++ Craps/src/application/model/Game.java
@@ -1,40 +1,41 @@
 package application.model;
 
 public class Game {
-	private Die d1;
+	//this is the Game class that has the 2 dice and where the player information is held
+	private Die d1; //calls the Die class
 	private Die d2;
-	private Player player;
+	private Player player; //calls the Player class
 	
-	public Game() {
+	public Game() { //constructor
 		d1 = new Die();
 		d2 = new Die();
 	}
 
-	public Die getD1() {
+	public Die getD1() { //gets the information about die 1
 		return d1;
 	}
 
-	public void setD1(Die d1) {
+	public void setD1(Die d1) { //then sets the value to die 1
 		this.d1 = d1;
 	}
 
-	public Die getD2() {
+	public Die getD2() { //gets the value for die 2
 		return d2;
 	}
 
-	public void setD2(Die d2) {
+	public void setD2(Die d2) { //then sets the value for die 2
 		this.d2 = d2;
 	}
 
-	public Player getPlayer() {
+	public Player getPlayer() { //get the player information
 		return player;
 	}
 
-	public void setPlayer(Player player) {
+	public void setPlayer(Player player) { //set the player information
 		this.player = player;
 	}
 	
-	public void roll() {
+	public void roll() { //rolls the dice 
 		d1.roll();
 		d2.roll();
 	}
